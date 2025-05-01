664d40c3eb0991debc5accf576e1be13044611dc
Derek Stritzel <dstritzel@user.noreply.github.com>
2025-05-01 11:57:04 -0400

    Move to user based using brew

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# linux-setup
-Linux Configs and scripts for user/OS setup.
+# New terminal setup for Linux/Mac
+
+## Description
+
+Linux/Mac Configs and scripts for user setup.
+
+## Requirements
+
+* zsh - Might be default in most distros and for mac
+* build - items for brew and pyenv (LINUX) TODO
+* xcode - I think brew needs xcode for building packages
--- setup-user.sh
+++ setup-user.sh
@@ -1,43 +1,68 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
 # TODO: Consider Using .env file and generate values from there
-GIT_USER="dstritzel"
+GIT_USER=${GIT_USER:-"dstritzel"}
+PYTHON_VERSION=${PYTHON_VERSION:-"3.13.3"}
+NODE_VERSION=${NODE_VERSION:-"--lts"}
+BREW_PACKAGES=${BREW_PACKAGES:-"pyenv pyenv-virtualenv nvm neovim gh"}
+PIP_PACKAGES=${PIP_PACKAGES:-"neovim"}
 
-# TODO: Double check zsh location then set
-chsh -s /usr/bin/zsh
+# Output Configs/Env
+echo "Github User: $GIT_USER"
+echo "Python Version: $PYTHON_VERSION"
 
-# User Setup nvim
-rm -rf ~/.config/nvim
-cp -r ./nvim ~/.config/
+exit
 
-if [ ! -d ~/.ssh ]; then
-  echo ".ssh does not exist"
-  mkdir ~/.ssh
+# Install OMZ info
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp omz/zshrc $HOME/.zshrc
+cp omz/zprofile $HOME/.zprofile
+. $HOME/.zshrc
+
+# Install Brew
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # TODO: Skip Kitty if there is no GUI
+  ## Kitty Install - Ubuntu 24.04 Kitty is borked with nvim
+  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+  ln -sf $HOME/.local/kitty.app/bin/kitty $HOME/.local/kitty.app/bin/kitten $HOME/.local/bin/
+  cp $HOME/.local/kitty.app/share/applications/kitty.desktop $HOME/.local/share/applications/
+  cp $HOME/.local/kitty.app/share/applications/kitty-open.desktop $HOME/.local/share/applications/
+  sed -i "s|Icon=kitty|Icon=$(readlink -f $HOME)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" $HOME/.local/share/applications/kitty*.desktop
+  sed -i "s|Exec=kitty|Exec=$(readlink -f $HOME)/.local/kitty.app/bin/kitty|g" $HOME/.local/share/applications/kitty*.desktop
+  echo 'kitty.desktop' >$HOME/.config/xdg-terminals.list
+  # Homebrew Install - We don't want to require root without good reason
+  git checkout https://github.com/Homebrew/brew $HOME/.linuxbrew
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  # Homebrew Install
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # Install Kitty
+  brew install kitty
+else
+  echo "Unknown OS = $OSTYPE"
 fi
 
-cp ssh/config ~/.ssh/
+. $HOME/.zshrc
 
-# Only run if not system user
-#if [[ $EUID -gt 999 ]]; then
-curl https://github.com/${GIT_USER}.keys | tee -a ~/.ssh/authorized_keys
-#fi
+# User Setup nvim
+rm -rf $HOME/.config/nvim
+cp -r ./nvim $HOME/.config/
 
-## Kitty Install - Ubuntu 24.04 Kitty is borked with nvim
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
-cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
-sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
-echo 'kitty.desktop' >~/.config/xdg-terminals.list
+if [ ! -d $HOME/.ssh ]; then
+  echo ".ssh does not exist"
+  mkdir $HOME/.ssh
+fi
 
-# Pyenv
-curl -fsSL https://pyenv.run | bash
+cp ssh/ssh_config $HOME/.ssh/config
+
+curl https://github.com/${GIT_USER}.keys | tee -a $HOME/.ssh/authorized_keys
+
+# Brew Installs
+brew install ${BREW_PACKAGES}
 
 # NVM and Node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
-nvm install --lts
-nvm use --lts
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+nvm install ${NODE_VERSION}
+nvm use ${NODE_VERSION}
 
 # Setup GIT
 git config --global user.name "${GIT_USER}"
